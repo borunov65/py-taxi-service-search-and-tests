05501bd8f9b69ba114bfe9ede84ad9ee9dda5960
Ihor Borunov <borunov65@gmail.com>
2025-07-30 22:41:41 +0200

    Solution

--- taxi/tests/test_forms.py
+++ taxi/tests/test_forms.py
@@ -15,7 +15,19 @@ class FormsTests(TestCase):
         }
         form = DriverCreationForm(data=form_data)
         self.assertTrue(form.is_valid())
-        self.assertEqual(form.cleaned_data["username"], form_data["username"])
-        self.assertEqual(form.cleaned_data["first_name"], form_data["first_name"])
-        self.assertEqual(form.cleaned_data["last_name"], form_data["last_name"])
-        self.assertEqual(form.cleaned_data["license_number"], form_data["license_number"])
+        self.assertEqual(
+            form.cleaned_data["username"],
+            form_data["username"]
+        )
+        self.assertEqual(
+            form.cleaned_data["first_name"],
+            form_data["first_name"]
+        )
+        self.assertEqual(
+            form.cleaned_data["last_name"],
+            form_data["last_name"]
+        )
+        self.assertEqual(
+            form.cleaned_data["license_number"],
+            form_data["license_number"]
+        )
