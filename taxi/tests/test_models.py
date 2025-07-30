aec61a9cad98e8749c781e4bba18ca184887d5fd
Ihor Borunov <borunov65@gmail.com>
2025-07-30 22:29:49 +0200

    Solution

--- taxi/tests/test_models.py
+++ taxi/tests/test_models.py
@@ -41,7 +41,7 @@ class ModelsTests(TestCase):
     def test_create_driver_with_license_number(self):
         username = "test"
         password = "test123"
-        license_number = "Test license_number"
+        license_number = "ABC12345"
         driver = get_user_model().objects.create_user(
             username=username,
             password=password,
